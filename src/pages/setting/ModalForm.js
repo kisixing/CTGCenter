d055a382ab1f264b6bf588fac4ca0935af252c15
lxf <lixiaofan.dev@gmail.com>
2020-07-09 17:34:27 +0800

    settting

--- src/pages/setting/ModalForm.js
+++ src/pages/setting/ModalForm.js
@@ -1,19 +1,37 @@
 import React, { useEffect, useState } from 'react';
 import { request } from "../../common/request";
-import { Modal, Form, } from 'antd';
-export function ModalForm({ visible }) {
+import { Modal, Form, Input, message, } from 'antd';
+export function ModalForm({ visible, dataItem, onCancel, onOk, setVisible }) {
+    const isEdit = !!dataItem
     const [form] = Form.useForm()
     useEffect(() => {
-    }, [])
+        form.setFieldsValue(dataItem || { value: '', key: '' })
+    }, [dataItem, form])
+    const submit = () => {
+        form.validateFields().then(data => {
+            request[dataItem ? 'put' : 'post']('/runtime-properties/', { data }).then(() => {
+                message.info('操作成功！')
+                setVisible(false)
+            })
+
+        })
+    }
     return (
         <>
-            <Modal footer={null} visible={visible} width={400}>
-                <Form form={form}>
-                    <Form.Item name="字段名">
+            <Modal visible={visible} width={400}
+                onCancel={() => {
+                    setVisible(false)
+                }}
+                onOk={submit}
+                title={isEdit ? '编辑' : '新增'}
 
+            >
+                <Form form={form} onFinish={submit} labelCol={{ xs: 4 }} >
+                    <Form.Item name="key" label="字段名" rules={[{ required: true }]}>
+                        <Input />
                     </Form.Item>
-                    <Form.Item name="字段名">
-
+                    <Form.Item name="value" label="值" rules={[{ required: true }]}>
+                        <Input />
                     </Form.Item>
                 </Form>
             </Modal>
--- src/pages/setting/index.js
+++ src/pages/setting/index.js
@@ -2,34 +2,79 @@ import ReactDOM from 'react-dom';
 import styles from './index.css';
 import { useEffect, useState } from 'react';
 import { request } from '../../common/request';
-import { Table } from 'antd';
-
+import { Table, Button, message, Divider } from 'antd';
+import { ModalForm } from "./ModalForm";
 function App() {
   const [list, setList] = useState([])
+  const [visible, setVisible] = useState(false)
+  const [dataItem, setDataItem] = useState(null)
   useEffect(() => {
     const receiveMessage = (event) => {
 
       if (event.data) {
-        console.log(event.data);//孩子, 我来看你了
-
+        console.log(event.data);
       }
 
     };
     window.addEventListener('message', receiveMessage, false);
+    fetchData()
 
+  }, [])
+  function fetchData() {
     request.get('/runtime-properties/').then(r => {
       setList(r)
     })
-  }, [])
+  }
+  function del() {
+    request.delete('/runtime-properties/', { data: dataItem }).then(() => {
+      message.info('删除成功！')
+    })
+  }
+  function onOk() {
+    setVisible(false)
+  }
+  function onCancel() {
+    setVisible(false)
+  }
+  useEffect(() => {
+    if (!visible) {
+      fetchData()
+      setDataItem(null)
+    }
+  }, [visible])
+  useEffect(() => {
+    if (dataItem) {
+      setVisible(true)
+    }
+  }, [dataItem])
   return (
     <div className={styles.normal}>
-      
+      <Button onClick={() => setVisible(true)} style={{ marginBottom: 12, float: 'right' }} type="primary">新增</Button>
+
       <Table dataSource={list} size="small" bordered columns={[
-        {title:'字段名',dataIndex:'key'},
-        {title:'值',dataIndex:'value'},
+        { title: '字段名', dataIndex: 'key' },
+        { title: '值', dataIndex: 'value' },
+        {
+          title: '操作', dataIndex: 'action', render(a, item, index) {
+            return (
+              <div>
+                <Button
+                  onClick={() => {
+                    setDataItem(item)
+                  }}
+
+                  type="link"
+                >编辑</Button>
+                <Divider type="vertical" />
+                <Button type="link" onClick={del}>删除</Button>
+              </div>
+            )
+          }
+        }
       ]}>
 
       </Table>
+      <ModalForm visible={visible} dataItem={dataItem} onCancel={onCancel} onOk={onOk} setVisible={setVisible} />
     </div>
   );
 }
